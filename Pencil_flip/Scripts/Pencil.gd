5aa6864ab387e362b0f069d3005ed26d905bece2
Splatman86 <120541361+Splatman86@users.noreply.github.com>
2025-01-28 09:15:05 -0700

    Attacking Fully works
    Adds flipping the pencil so you can also use it when attacking
    
    POSSIBLE CHANGES:
    
    - Make pencil longer (more spear-esq)?
    - Change attack speed? We'll test for balancing down the line

--- Pencil_flip/Scripts/Pencil.gd
+++ Pencil_flip/Scripts/Pencil.gd
@@ -1,12 +1,19 @@
 extends AnimatableBody2D
 
-
+var Direction = "Right"
 
 func _process(float) -> void:
+	
+	# Handles attacking
 	if Input.is_action_just_pressed("Attack"):
-		print("I am attacking")
-		$AnimationPlayer.play("Attack")
+		if Direction == "Left":
+			$AnimationTree/AnimationPlayer.play("Attack Left")
+		elif Direction == "Right":
+			$AnimationTree/AnimationPlayer.play("Attack Right")
+		# Switches the direction the pencil is facing, as well as the attacking direction. Works for Left and Right.
 	if Input.is_action_just_pressed("Move_Left"):
-		$Sprite2D.set_flip_h(1)
+		Direction = "Left"
+		$AnimatedSprite2D.set_frame(0)
 	if Input.is_action_just_pressed("Move_Right"):
-		$Sprite2D.set_flip_h(0)
+		Direction = "Right"
+		$AnimatedSprite2D.set_frame(1)
