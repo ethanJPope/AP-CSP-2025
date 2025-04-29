94af4653ca9f1953fe6a9c0ee522d1053a81387c
Splatman86 <120541361+Splatman86@users.noreply.github.com>
2025-04-29 15:10:19 -0700

    get this done so i can figure it out later
    audrey named this lol

--- poker-tracker/Global.gd
+++ poker-tracker/Global.gd
@@ -1,10 +1,14 @@
 extends Node
 
+
 var Players = 7
 var Pot = 0
-var X
-var Points = {
-	X: 100
-	
-	
-}
+var Player1Name
+var Player2Name
+var Player3Name
+var Player4Name
+var Player5Name
+var Player6Name
+var Player7Name
+var Player8Name
+var Points = {Player1Name: 100}
--- poker-tracker/main.gd
+++ poker-tracker/main.gd
@@ -28,3 +28,34 @@ func _ready() -> void:
 		$P8.hide()
 	elif Global.Players == 7:
 		$P8.hide()
+
+
+
+# SIGNALS
+
+# Name setters
+func _on_player_name_text_changed():
+	Global.Player1Name = $"P1/Player Name".text
+	print(Global.Points)
+
+func _on_player_2_name_text_changed():
+	Global.Player2Name = $"P2/Player 2 Name".text
+
+func _on_player_3_name_text_changed():
+	Global.Player3Name = $"P3/Player 3 Name".text
+
+func _on_player_4_name_text_changed():
+	Global.Player4Name = $"P4/Player 4 Name".text
+
+func _on_player_5_name_text_changed():
+	Global.Player5Name = $"P5/Player 5 Name".text
+
+func _on_player_6_name_text_changed():
+	Global.Player6Name = $"P6/Player 6 Name".text
+
+func _on_player_7_name_text_changed():
+	Global.Player7Name = $"P7/Player 7 Name".text
+
+func _on_player_8_name_text_changed():
+	Global.Player8Name = $"P8/Player 8 Name".text
+	print(Global.Points)
